6116378839ad1dbc82e465ed699e96a9fc42f92b
chenweiyang <chenweiyang@gmail.com>
2015-09-07 16:04:47 +0800

    加入显示天气信息的功能.

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.example.coolweather.R;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -70,6 +73,13 @@ public class ChooseAreaActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -87,10 +97,16 @@ public class ChooseAreaActivity extends Activity{
 				}else if (currentLevel == LEVEL_CITY) {
 					selectedCity = citiyList.get(index);
 					queryCounties();
+				}else if (currentLevel == LEVEL_COUNTY) {
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
-		queryProvinces();
+		queryProvinces();//加载省级数据
 	}
 	
 	/**
@@ -158,7 +174,7 @@ private void queryProvinces() {
 	private void queryFromServer(final String code, final String type){
 		String address;
 		if (!TextUtils.isEmpty(code)) {
-			address = "http://wwww.weather.com.cn/data/list3/city" + code +".xml";
+			address = "http://www.weather.com.cn/data/list3/city" + code +".xml"; //  - -   
 		}else{
 			address = "http://www.weather.com.cn/data/list3/city.xml";
 		}
@@ -199,6 +215,7 @@ private void queryProvinces() {
 			@Override
 			public void onError(Exception e) {
 				//通过runOnUIThread()方法回到主线程处理逻辑
+				e.printStackTrace();
 				runOnUiThread(new Runnable() {
 					
 					@Override
